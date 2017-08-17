09bbdec95e434effc8f799b106f275570d618065
Juan Manuel <jmouriz@gmail.com>
2017-08-17 01:12:57 -0300

    Some fixes on event data trasnfer handling

--- example/js/sample.controllers.js
+++ example/js/sample.controllers.js
@@ -2,17 +2,25 @@ angular
   .module('MyApp')
   .controller('sampleCtrl', sampleCtrl);
 
-
-function sampleCtrl($scope, $http){
-
-    // prepare the widget data
-    $http.get("/example/data/engine.json").success(function(data){
-        $scope.data = {};
-        $scope.data.WEconfiguration = data;
-        $scope.data.WEcallback = function(e, configuration){
-            console.log(e, configuration);
-        };
-    });
+function sampleCtrl($scope, $http) {
+   // prepare the widget data
+   var set_data = function(data) {
+      $scope.data = {};
+      $scope.data.WEconfiguration = data;
+      $scope.data.WEcallback = function(e, configuration){
+         console.log(e, configuration);
+      };
+   }
+   var promise = $http.get("example/data/engine.json");
+   if (promise.success) {
+      promise.success(function(data) {
+         set_data(data);
+      });
+   } else {
+      promise.then(function(response) {
+         set_data(response.data);
+      });
+   }
 
 
 }
